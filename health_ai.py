f5c4dee2ae7c202be507845940ad4e6bbc672c2e
Datttabk <dattakambagi021@gmail.com>
2025-05-29 23:52:37 +0530

    health_ai.py

--- health_ai.py
+++ health_ai.py
@@ -2,6 +2,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import joblib
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
